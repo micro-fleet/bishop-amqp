ba48f09cc598f6e025512868f241a0019e045594
Anfey <vkfont@yandex.ru>
2017-03-10 15:16:32 +0300

    Current progress

--- test/transport.js
+++ test/transport.js
@@ -12,27 +12,35 @@ const Bishop = require('bishop')
 const Promise = require('bluebird')
 const transport = require(process.env.PWD)
 
-// /*
-// { heartbeat: '10000',
-//   reconnect: 'true',
-//   reconnectDelayTime: '1000',
-//   hostRandom: 'false',
-//   connectTimeout: '30000',
-//   clientProperties: '...',
-//   ssl: 'false',
-//   sslOptions: '...',
-//   temporaryChannelTimeout: '2000',
-//   noDelay: 'true',
-//   proto: 'amqp:',
-//   host: 'localhost',
-//   port: '5672',
-//   path: '/',
-//   login: 'guest',
-//   password: 'guest' }
-//  */
-//
 test('test', async t => {
   const bishop = new Bishop()
-  await bishop.use(transport)
-  await Promise.delay(500)
+  await bishop.use(transport, {
+    roles: ['emitter']
+  })
+  bishop.add('role:test, act:eventemitter, some, parameters', () => {
+    return 'done'
+  })
+  const res = await bishop.act('role:test, act:eventemitter, some: arg, parameters: true')
+  console.log(res)
 })
+// test('test', async t => {
+//   const bishopServer = new Bishop()
+//   const bishopClient = new Bishop()
+//
+//   await bishopClient.use(transport, {
+//     name: 'amqp-client',
+//     routingKey: 'some.stuff',
+//     pattern: 'some:stuff',
+//     role: 'client'
+//   })
+//   await bishopServer.use(transport, {
+//     name: 'amqp-server',
+//     routingKey: 'some.stuff',
+//     pattern: 'some:stuff',
+//     role: 'server'
+//   })
+//   bishopServer.add('some:stuff', () => 'hello')
+//   await bishopClient.act('some:stuff, with:stuff')
+//
+//   await Promise.delay(500)
+// })
